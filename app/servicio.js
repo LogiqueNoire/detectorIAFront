e1720c689c661d0671663ef7673b27245e98aa93
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-11-15 23:37:30 -0500

    feat: :sparkles: preparando para futuros modelos

--- app/servicio.js
+++ app/servicio.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const URL = process.env.NEXT_PUBLIC_URL_BACK
 
-export async function enviarArchivo(file) {
+export async function procesarSVM(file) {
   const formData = new FormData();
   formData.append("file", file);
 
   try {
     const response = await axios.post(
-      URL,
+      `${URL}/predict`,
       formData,
       {
         headers: {
@@ -17,8 +17,48 @@ export async function enviarArchivo(file) {
       }
     );
 
+    return response.data;
+  } catch (error) {
+    console.error("Error al enviar archivo:", error);
+  }
+}
 
-    console.log("Respuesta del servidor:", response.data);
+export async function procesarRoBERTa(file) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await axios.post(
+      `${URL}/predict/roberta`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error al enviar archivo:", error);
+  }
+}
+
+export async function procesarSimpleNet(file) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await axios.post(
+      `${URL}/predict/simplenet`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
     return response.data;
   } catch (error) {
     console.error("Error al enviar archivo:", error);
