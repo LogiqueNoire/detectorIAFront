be9079f9ca18e1ebc6743682998ccc88ad8d2e9a
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-11-16 23:46:26 -0500

    styles: nuevo diseño

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
-import { procesarSVM, procesarRoBERTa, procesarSimpleNet } from "@/app/servicio"
+import { procesarSVM, procesarRoBERTa, procesarMLP } from "@/app/servicio"
 import "./progressBar.css"
 import { Bird, Fish, Snail } from "lucide-react";
 
@@ -12,13 +12,14 @@ export default function Home() {
   const [isLooping2, setIsLooping2] = useState(false);
   const [isLooping3, setIsLooping3] = useState(false);
   const [percent, setPercent] = useState(0)
+  const [filename, setFileName] = useState("")
   const [result, setResult] = useState<{ predicted_label: number; prob: number } | null>(null);
   const [selectedModel, setSelectedModel] = useState<number>(0);
   const models = ["snil", "fish", "bird"]
 
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const frase = "Procesando documento..."
+  const frase = "Procesando archivo..."
 
   useEffect(() => {
     let interval1: string | number | NodeJS.Timeout | undefined;
@@ -34,7 +35,7 @@ export default function Home() {
             return prev;
           }
         });
-      }, 200);
+      }, 50);
 
       interval2 = setInterval(() => {
         setPercent(prev => {
@@ -45,7 +46,7 @@ export default function Home() {
             return prev;
           }
         });
-      }, 100);
+      }, 20);
     } else {
       setProgress("");
       setPercent(0);
@@ -93,98 +94,131 @@ export default function Home() {
       result = await procesarRoBERTa(file)
     }
     if (selectedModel == 2) {
-      result = await procesarSimpleNet(file)
+      result = await procesarMLP(file)
     }
     if (result == null) { return }
     setResult(result)
+    setFileName(file.name)
     setIsDragging(false)
   }
 
   return (
-    <div className="flex flex-col gap-7 min-h-screen items-center justify-center bg-linear-to-tr/hsl from-sky-950 from-20% to-cyan-950 to-80%  font-sans px-5">
-      <h1 className="momo-trust-display-regular text-4xl text-amber-400 text-center">Detector de IA en textos académicos</h1>
-      {progress.length == 0 &&
-        <div className="border-amber-400 border-2 p-7 rounded-2xl">
-          <div className="border-cyan-400 border-4 border-dotted p-7 rounded-xl hover:bg-cyan-400/5"
-            onDragOver={(e) => { e.preventDefault(); setIsDragging(true); }}
-            onDragLeave={(e) => { e.preventDefault(); setIsDragging(false); }}
-            onDrop={(e) => {
-              e.preventDefault()
-              setIsDragging(false)
-              const file = e.dataTransfer.files?.[0]
-              if (file.type === "application/pdf" || file.name.endsWith(".txt")) {
-                handleUpload(file) // <-- ahora funciona sin convertir el evento
-              }
-            }}>
-            <p className="momo-trust-display-regular text-cyan-400 text-xl text-center mb-5">Sube ingresa o arrasta un archivo txt o pdf</p>
-            <div className="flex justify-center">
-              <button className="momo-trust-display-regular text-center text-white text-base border-2 border-white p-2 rounded-lg hover:bg-white/10"
-                onClick={() => fileInputRef.current?.click()}>Subir archivo</button>
-            </div>
-            <input type="file" accept=".txt,.pdf" className="hidden" ref={fileInputRef} onChange={handleUpload}></input>
+    <div className="flex flex-col gap-7 min-h-screen items-center justify-center bg-linear-to-tr/hsl from-slate-50 from-20% to-slate-100 to-80%  font-sans px-6">
+      <div id="lienzo_fijo" className="w-480 h-120 fixed top-0 left-1/2 -translate-x-1/2 pointer-events-none">
 
-          </div>
-          <div className="flex flex-col p-5">
-            <p className="momo-trust-display-regular text-white text-xl text-center mb-5">Elige el modo de detección</p>
-            <div className="flex justify-center gap-5">
-              <button className={`momo-trust-display-regular text-center text-white text-base border-2 border-white p-2 rounded-lg ${selectedModel == 0 ? "bg-white/15" : ""} hover:bg-white/5`}
-                onClick={() => setSelectedModel(0)}>
-                <div className="flex flex-col p-2 gap-2">
-                  <Snail className="w-10 h-10"></Snail>
-                  Snail
-                </div>
-              </button>
-              {/*<button className={`momo-trust-display-regular text-center text-white text-base border-2 border-white p-2 rounded-lg ${selectedModel == 1 ? "bg-white/15" : ""} hover:bg-white/5`}
-                onClick={() => setSelectedModel(1)}>
-                <div className="flex flex-col p-2 gap-2">
-                  <Fish className="w-10 h-10"></Fish>
-                  Snail
-                </div>
-              </button>
-              <button className={`momo-trust-display-regular text-center text-white text-base border-2 border-white p-2 rounded-lg ${selectedModel == 2 ? "bg-white/15" : ""} hover:bg-white/5`}
-                onClick={() => setSelectedModel(2)}>
-                <div className="flex flex-col p-2 gap-2">
-                  <Bird className="w-10 h-10"></Bird>
-                  Bird
-                </div>
-              </button>*/}
-            </div>
-          </div>
-        </div>}
-      {progress != undefined && progress.length > 0 && progress.length <= frase.length &&
-        <div className="progress-wrapper">
-          <div className="progress-text">{progress}</div>
-          <div className={`water-fill`} style={{ width: `${percent}%`, height: '100%' }}>
-            <div className={`wave wave1 ${isLooping ? 'loop' : ''}`}></div>
-            <div className={`wave wave2 ${isLooping2 ? 'loop' : ''}`}></div>
-            <div className={`wave wave3 ${isLooping3 ? 'loop' : ''}`}></div>
-            <div className="bubbles"></div>
-          </div>
-        </div>}
-      {result &&
-        <div className="bg-linear-to-tr from-white/50 to-white/70 rounded-2xl p-5">
-          <table>
-            <thead>
-              <th className="pb-3">
-                <div className="momo-trust-display-regular text-lg text-sky-950 text-left">Resultado</div>
-              </th>
-              <th className="pb-3">
-                <div className="momo-trust-display-regular text-lg text-right ps-8">{result.predicted_label == 0 ? "Escrito por humano" : "Escrito con IA"}</div>
-              </th>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="">
-                  <div className="momo-trust-display-regular text-lg text-sky-950">Confianza</div>
-                </td>
-                <td className="">
-                  <div className="momo-trust-display-regular text-lg text-right">{(Number(result.prob.toFixed(4)) * 100).toFixed(2)}%</div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div id="derecha_atras" className="w-65 h-30 bg-amber-300 absolute z-0 rotate-75 top-70 left-265"></div>
+        <div id="gordo_arriba" className="w-45 h-62 bg-lime-400 absolute z-0 rotate-15 top-48 left-233"></div>
+        <div id="echado_arriba" className="w-58 h-30 bg-amber-300 absolute z-1 rotate-355 top-20 left-210"></div>
+        <div id="cuadrado" className="w-45 h-62 bg-lime-400 absolute z-1 rotate-45 top-18 left-155"></div>
+        <div id="cuadrado" className="w-35 h-35 bg-amber-300 absolute z-1 rotate-45 top-55 left-155"></div>
+        <div id="cuadrado" className="w-45 h-62 bg-lime-400 absolute z-1 rotate-8 top-82 left-148"></div>
+        <div id="flaco_central" className="w-13 h-60 bg-amber-300 absolute z-1 rotate-342 top-100 left-230"></div>
+        <div id="largo_der" className="w-60 h-15 bg-amber-300 absolute z-2 rotate-25 top-135 left-262"></div>
+        <div id="cuadrado" className="w-25 h-42 bg-lime-400 absolute z-3 rotate-0 top-110 left-285"></div>
+        <div id="echado-ab" className="w-40 h-30 bg-lime-400 absolute z-3 rotate-15 top-170 left-185"></div>
+        <div id="pequeño" className="w-15 h-35 bg-amber-300 absolute z-3 rotate-12 top-135 left-185"></div>
+        <div id="echado_izq" className="w-25 h-50 bg-lime-400 absolute z-3 rotate-60 top-90 left-190"></div>
+        <div id="gordo_abajo" className="w-45 h-60 bg-lime-400 absolute z-4 rotate-25 top-135 left-235"></div>
+        <div id="echado_abajo" className="w-65 h-30 bg-amber-300 absolute z-4 rotate-355 top-185 left-175"></div>
 
-        </div>}
+      </div>
+
+
+      <div className="flex flex-col gap-7 min-h-screen items-center justify-center font-sans px-5 z-10">
+        <h1 className="momo-trust-display-regular text-4xl text-yellow-700 text-center">Detector de IA en textos académicos</h1>
+        {progress.length == 0 &&
+          <div className="border-yellow-700 border-3 p-7 rounded-2xl">
+            <div className="border-lime-600 border-4 border-dotted p-6 rounded-xl hover:bg-lime-600/10"
+              onDragOver={(e) => { e.preventDefault(); setIsDragging(true); }}
+              onDragLeave={(e) => { e.preventDefault(); setIsDragging(false); }}
+              onDrop={(e) => {
+                e.preventDefault()
+                setIsDragging(false)
+                const file = e.dataTransfer.files?.[0]
+                if (file.type === "application/pdf" || file.name.endsWith(".txt")) {
+                  handleUpload(file) // <-- ahora funciona sin convertir el evento
+                }
+              }}>
+              <p className="momo-trust-display-regular text-lime-600 text-xl text-center mb-5">Sube o arrasta un archivo .txt .pdf</p>
+              <div className="flex justify-center">
+                <button className="momo-trust-display-regular text-center text-black text-base border-3 border-black p-2 rounded-lg hover:bg-black/10"
+                  onClick={() => fileInputRef.current?.click()}>Subir archivo</button>
+              </div>
+              <input type="file" accept=".txt,.pdf" className="hidden" ref={fileInputRef} onChange={handleUpload}></input>
+
+            </div>
+            <div className="flex flex-col p-5">
+              <p className="momo-trust-display-regular text-black text-xl text-center mb-5">Elige el modo de detección</p>
+              <div className="flex justify-center gap-5">
+                <button className={`momo-trust-display-regular text-center text-cyan-600 text-base border-3 border-cyan-600 p-2 rounded-lg ${selectedModel == 0 ? "bg-cyan-300/25" : ""} hover:bg-white/30`}
+                  onClick={() => setSelectedModel(0)}>
+                  <div className="flex flex-col p-2 gap-2">
+                    <Fish className="w-10 h-10"></Fish>
+                    Fish
+                  </div>
+                </button>
+                {/*<button className={`momo-trust-display-regular text-center text-orange-600 text-base border-3 border-orange-600 p-2 rounded-lg ${selectedModel == 1 ? "bg-orange-600/20" : ""} hover:bg-orange-600/15`}
+                  onClick={() => setSelectedModel(1)}>
+                  <div className="flex flex-col p-2 gap-2">
+                    <Bird className="w-10 h-10"></Bird>
+                    Bird
+                  </div>
+                </button>*/}
+                <button className={`momo-trust-display-regular text-center text-violet-800 text-base border-3 border-violet-800 p-2 rounded-lg ${selectedModel == 2 ? "bg-violet-300/50" : ""} hover:bg-white/40`}
+                  onClick={() => setSelectedModel(2)}>
+                  <div className="flex flex-col p-2 gap-2">
+                    <Snail className="w-10 h-10"></Snail>
+                    Snail
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>}
+        {progress != undefined && progress.length > 0 && progress.length <= frase.length &&
+          <div className="progress-wrapper">
+            <div className="progress-text">{progress}</div>
+            <div className={`water-fill`} style={{ width: `${percent}%`, height: '100%' }}>
+              <div className={`wave wave1 ${isLooping ? 'loop' : ''}`}></div>
+              <div className={`wave wave2 ${isLooping2 ? 'loop' : ''}`}></div>
+              <div className={`wave wave3 ${isLooping3 ? 'loop' : ''}`}></div>
+              <div className="bubbles"></div>
+            </div>
+          </div>}
+        {result &&
+          <div className="bg-linear-to-tr from-white/50 to-white/70 rounded-2xl p-5">
+            <table>
+              <thead>
+                <tr>
+                  <th className="pb-3">
+                    <div className="momo-trust-display-regular text-lg text-lime-900 text-left">Nombre</div>
+                  </th>
+                  <th className="pb-3">
+                    <div className="momo-trust-display-regular text-lg text-right ps-8">{filename}</div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="pb-3">
+                    <div className="momo-trust-display-regular text-lg text-lime-900 text-left">Resultado</div>
+                  </td>
+                  <td className="pb-3">
+                    <div className="momo-trust-display-regular text-lg text-right ps-8">{result.predicted_label == 0 ? "Escrito por humano" : "Escrito con IA"}</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="">
+                    <div className="momo-trust-display-regular text-lg text-lime-900">Confianza</div>
+                  </td>
+                  <td className="">
+                    <div className="momo-trust-display-regular text-lg text-right">{(Number(result.prob.toFixed(4)) * 100).toFixed(2)}%</div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+          </div>}
+      </div>
     </div>
   );
 }
